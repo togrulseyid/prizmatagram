629341f469eea63b6bd86fc00317f9ceca96c795
Toghrul Seyidov <togrul.seyid93@gmail.com>
2017-03-19 21:43:59 +0100

    Added C++ libraries .so files

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,7 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions -fopenmp -DANDROID
 #APP_ABI := armeabi-v7a
-APP_ABI := all
+#APP_ABI := all
+APP_ABI := armeabi armeabi-v7a x86 mips
 APP_PLATFORM := android-24
-APP_CPPFLAGS += -std=c++11      #  Enable C++11. However, pthread, rtti and exceptions arent enabled
+APP_CPPFLAGS += -std=c++11      #  Enable C++11. However, pthread, rtti and exceptions aren't enabled
